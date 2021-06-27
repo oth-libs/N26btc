fb68a9ef0081688881c84c66f6af47e7c5be970e
Othman <eljazouli.othman@gmail.com>
2021-06-28 00:39:49 +0100

    no message

--- app/src/androidTest/java/io/n26btc/ErrorViewOnStartTest.kt
+++ app/src/androidTest/java/io/n26btc/ErrorViewOnStartTest.kt
@@ -10,6 +10,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.n26btc.domain.model.BitcoinChartType
 import io.n26btc.homepage.ChartFragment
+import io.n26btc.utils.Constants
+import io.n26btc.utils.Constants.CHART_FRAGMENT_ARG_CHART_TYPE
 import org.hamcrest.Matchers.not
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,7 +26,7 @@ class ErrorViewOnStartTest {
 
   @Test
   fun testWhenFragmentStartThenOnlyLoadingIsShown() {
-    val fragmentArgs = bundleOf("blaa" to BitcoinChartType.MARKET_PRICE.name)
+    val fragmentArgs = bundleOf(CHART_FRAGMENT_ARG_CHART_TYPE to BitcoinChartType.MARKET_PRICE.name)
     val scenario = launchFragmentInContainer<ChartFragment>(fragmentArgs)
 
     onView(withId(R.id.viewChartNoInternet)).check(matches(not(isDisplayed())))
